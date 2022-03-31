90bf1772ee49c1e624307c8daee834a1ba2938f8
smyrz1 <smyrz1@126.com>
2022-03-31 16:00:42 +0800

    1.项目骨架Springboot + MyBatis 2.Swagger-UI实现在线api文档 3.整合Redis实现缓存功能 4.整合SpringSecurity和JWT实现认证和授权

---
+++ src/test/java/com/example/malllearningzrl/MallLearningZrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.malllearningzrl;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallLearningZrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
